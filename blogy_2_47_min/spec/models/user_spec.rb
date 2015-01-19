2b50ea78118e797cfee8a94adacf45d3801f2a31
Delmer Reed <delmer814@yahoo.com>
2015-01-19 12:52:30 -0800

    basic user_spec outline

--- blogy_2_47_min/spec/models/user_spec.rb
+++ blogy_2_47_min/spec/models/user_spec.rb
@@ -1,5 +1,81 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "validate" do
+
+    describe "email" do 
+
+      describe "on create" do
+
+        it "should be present" do
+        end
+
+        it "should be confirmed" do
+        end
+
+        it "should enforce presence of email_confirmation" do
+        end
+
+        it "should be valid format" do
+        end
+
+      end
+
+      describe "on updates where !email.nil?" do
+
+        it "should be confirmed" do
+        end
+
+        it "should enforce presence of email_confirmation" do
+        end
+
+        it "should be valid format" do
+        end
+
+      end
+
+    end
+
+    describe "password" do 
+
+      describe "on create" do
+
+        it "should be present" do
+        end
+
+        it "should be confirmed" do
+        end
+
+        it "should enforce presence of confirmation" do
+        end
+
+        it "should have valid length" do
+        end
+
+        it "should be valid format" do
+        end
+
+      end
+
+      describe "on updates where !password.nil?" do
+
+        it "should be confirmed" do
+        end
+
+        it "should enforce presence of confirmation" do
+        end
+
+        it "should have valid length" do
+        end
+
+        it "should be valid format" do
+        end
+
+      end
+
+    end
+
+  end
+  
 end
