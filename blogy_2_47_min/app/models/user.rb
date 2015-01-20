c6c76d29a9ceb5bebddb4ddb0387fdce109c6bbf
Delmer Reed <delmer814@yahoo.com>
2015-01-19 17:37:47 -0800

    add user tests

--- blogy_2_47_min/app/models/user.rb
+++ blogy_2_47_min/app/models/user.rb
@@ -4,15 +4,33 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  validates_confirmation_of :email
-  validates_presence_of :password_confirmation
-  validates_presence_of :email_confirmation
-  validates_uniqueness_of :email
-  validates_presence_of :email
+  validates :email, 
+            uniqueness: true,
+            confirmation: true
+
+  validates :password_confirmation,
+            :presence => true,
+            :length => { :minimum => 8},
+            # password must not be all lowercase letters
+            :format => { :without => /\A[a-z]+\z/ },
+            :if => :password_required? 
+
+  validates :email_confirmation, 
+            format: { with: /.+@.+/ },
+            presence: true,
+            if: :email_conf_required?
+
 
   def self.confirm(email_param, password_param)
     user = find_by({email: email_param})
     user.try(:authenticate, password_param)
   end
 
+  def password_required?
+     self.new_record? or !self.password.nil?
+  end
+
+  def email_conf_required?
+     self.new_record? or self.email_changed?
+  end
 end
